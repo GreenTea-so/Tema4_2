9890830abafd36486661601224f02ee45ffe1936
GreenTea-so <lavrensepatton@gmail.com>
2021-05-28 13:59:42 +0300

    two commit blanders

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import './style/scss/scss.scss'
-import image_koz from './image/koz.png'
-import image_dog from './image/dog.png'
-import image_steve from './image/steve.png'
-import image_limur from './image/limur.png'
-import image_girl from './image/girl.png'
-import image_car from './image/car.png'
-import image_phone from './image/phone.png'
-import image_bbc from './image/bbc.png'
-import image_html5 from './image/html5.png'
-import image_html6 from './image/html6.png'
-import image_node from './image/node.png'
-import image_php from './image/php.png'
-import image_sql from './image/sql.png'
-import image_2 from './image/image_2.png'
-import image_3 from './image/image_3.png'
-
-import './script.js'
-
-
-
+import './style/scss/scss.scss';
+import './image/koz.png';
+import './image/dog.png';
+import './image/steve.png';
+import './image/limur.png';
+import './image/girl.png';
+import './image/car.png';
+import './image/phone.png';
+import './image/bbc.png';
+import './image/html5.png';
+import './image/html6.png';
+import './image/node.png';
+import './image/php.png';
+import './image/sql.png';
+import './image/image_2.png';
+import './image/image_3.png';
+import './script.js';
--- src/script.js
+++ src/script.js
@@ -1,29 +1,23 @@
-let offset = 0;
-const sliderLine = document.querySelector('.slider-line');
-
 function next(n) {
-    const image_1 = document.getElementById("image_1")
-    const image_2 = document.getElementById("image_2")
-    const image_3 = document.getElementById("image_3")
-    
-    if(n === 1){
-        image_1.style.display = "block"
-        image_2.style.display = "none"
-        image_3.style.display = "none"
-    }
-    if(n === 2){
-        image_1.style.display = "none"
-        image_2.style.display = "block"
-        image_3.style.display = "none"
-    }
-    if(n === 3){
-        image_1.style.display = "none"
-        image_2.style.display = "none"
-        image_3.style.display = "block"
-    }
+  const image1 = document.getElementById('image_1');
+  const image2 = document.getElementById('image_2');
+  const image3 = document.getElementById('image_3');
 
+  if (n === 1) {
+    image1.style.display = 'block';
+    image2.style.display = 'none';
+    image3.style.display = 'none';
+  }
+  if (n === 2) {
+    image1.style.display = 'none';
+    image2.style.display = 'block';
+    image3.style.display = 'none';
+  }
+  if (n === 3) {
+    image1.style.display = 'none';
+    image2.style.display = 'none';
+    image3.style.display = 'block';
+  }
 }
 
-
-
-window.next = next
+window.next = next;
