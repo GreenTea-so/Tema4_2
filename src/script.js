bc8efe1366b7465c3031864a7d13bb4978bdb0ef
GreenTea-so <lavrensepatton@gmail.com>
2021-06-09 20:09:08 +0300

    result commit

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 
 function next(n) {
   const image1 = document.getElementById('image_1');
@@ -25,31 +25,25 @@ function next(n) {
 window.next = next;
 
 function getResponse(quantityContent) {
- 
   const promiseBekon = axios.get('https://baconipsum.com/api/', {
     params: {
       type: 'all-meat',
-      paras: quantityContent
+      paras: quantityContent,
 
-    }
+    },
   });
 
   const promisePicsum = axios.get('https://picsum.photos/v2/list', {
-      params: {
-        limit: quantityContent,
-        width: 100
-      }
+    params: {
+      limit: quantityContent,
+      width: 100,
+    },
   });
-  
+
   Promise.all([promiseBekon, promisePicsum]).then((response) => {
-    console.log(response)
-    const contentBekon = response[0].data
-    const contentPicsum = response[1].data
-    console.log('content',contentPicsum)
-    console.log(contentPicsum[0].url)
-    
-    
-    
+    const contentBekon = response[0].data;
+    const contentPicsum = response[1].data;
+
     const parentClass = document.getElementsByClassName('container');
     const parent = parentClass[0];
 
@@ -66,15 +60,8 @@ function getResponse(quantityContent) {
       element.style.marginRight = '20px';
       element.innerHTML = `<div class="container_content_top"><div class="container_content_top_square ${yellowColor}"></div><h6>ABOUT SUPER LOGO</h6><div class="container_content_top_rectangle  ${yellowColor}"></div></div><div class="container_content_bottom"><div class="container_content_bottom_image"><img src="${contentPicsum[i].download_url}"></div><div class="container_content_bottom_text"><p>${contentBekon[i]}</p><a>Read more...</a></div></div>`;
       parent.appendChild(element);
-  }
+    }
   });
-
-
-  
-  
-  
-
-  
 }
 
 document.addEventListener('DOMContentLoaded', getResponse(12));
